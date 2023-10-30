22dff995926c0796dbc4777a4346e32d55d77592
PenVsse <vusiphu20042000@gmail.com>
2023-10-31 03:06:27 +0700

    up

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,6 @@
 const passport = require("passport");
+const bcrypt = require("bcrypt");
+const Users = require("../model/user");
 class userController {
   getLoginPage(req, res, next) {
     res.render("login/index", {
@@ -10,6 +12,49 @@ class userController {
       successRedirect: "/orchids",
       failureRedirect: "/user/login",
     })(req, res, next);
+  }
+  getRegisterPage(req, res, next) {
+    res.render("register/index", {
+      title: "Register",
+    });
+  }
+  async createAccount(req, res, next) {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      req.flash("error_msg", "Please enter all fields");
+      res.redirect("/user/register");
+      return;
+    }
+    const patternUsername = /^(?!^\s$)[^\s]{1,12}$/;
+    const patternPassword = /^(?!^\s$)[^\s]{6,12}$/;
+    const isValidUsername = patternUsername.test(username);
+    const isValidPassword = patternPassword.test(password);
+    if (!isValidUsername) {
+      req.flash("error_msg", "Please enter between 1 and 12 characters and do not contain spaces");
+      res.redirect("/user/register");
+      return;
+    }
+    if (!isValidPassword) {
+      req.flash("error_msg", "Please enter between 6 and 12 characters and do not contain spaces");
+      res.redirect("/user/register");
+      return;
+    }
+
+    const account = await Users.findOne({ username: username });
+    if (account) {
+      req.flash("error_msg", "Account already exists!");
+      res.redirect("/user/register");
+      return;
+    }
+    bcrypt.hash(password, 10, async function (err, hash) {
+      await Users.create({
+        username,
+        password: hash,
+      });
+      req.flash("success_msg", "Create account successfully!");
+      res.redirect("/user/login");
+      return;
+    });
   }
 }
 module.exports = new userController();
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,5 +3,6 @@ var router = express.Router();
 const userController = require("../controller/userController");
 
 router.route("/login").get(userController.getLoginPage).post(userController.loginAccount);
+router.route("/register").get(userController.getRegisterPage).post(userController.createAccount);
 
 module.exports = router;
